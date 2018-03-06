fe223c73cdf0f39e9bf6da7dae249417c2a33c7a
Austin Kabiru <austin.kabiru@andela.com>
2018-03-06 23:11:18 +0300

    add cli task

---
+++ cli.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require_relative 'quizzer'
+
+Quizzer::CliController.call(ARGV)
---
+++ quizzer.rb
@@ -0,0 +1,9 @@
+module Quizzer
+  class CliController
+    class << self
+      def call(args)
+        puts args
+      end
+    end
+  end
+end
